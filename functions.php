aa328916f93814c60a71288238d6a1d21e5d534a
AbdDan <36032143+AbdDan@users.noreply.github.com>
2020-12-09 11:09:50 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -38,6 +38,24 @@ return $pdo->lastInsertId();
 }
 
 
+function login ($email, $password) {
+	$pdo = new PDO("mysql:host=localhost;port=3307;dbname=tasks_db;", "root", "");
+	$sql = "SELECT * FROM ttest WHERE email=:email";
+    $statement = $pdo->prepare($sql);
+    $statement->execute([
+        'email'  => $email
+    ]);
+    $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+    if(password_verify($password, $result['password'])) {
+        return $result;
+    } else {
+        return false;
+    }
+
+}
+
+
 function display_flash_message($name) {
     if (isset($_SESSION[$name])) {
         echo "<div class=\"alert alert-{$name} text-dark\" role=\"alert\">{$_SESSION[$name]}</div>";
