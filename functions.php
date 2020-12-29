abd32e995ea51a43175bdda4d21225f1a9114216
AbdDan <36032143+AbdDan@users.noreply.github.com>
2020-12-29 11:24:30 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,10 +4,10 @@ session_start();
 function get_user_by_email($email)
 {
     //подключаемся к БД
-    $pdo = new PDO("mysql:host=localhost;port=3307;dbname=tasks_db;", "root", "");
+    $pdo = new PDO("mysql:host=localhost;port=3307;dbname=well;", "root", "");
 
     //создаем запрос
-    $sql = "SELECT * FROM ttest WHERE email=:email";
+    $sql = "SELECT * FROM new WHERE email=:email";
     $statement = $pdo->prepare($sql);
     $statement->execute(["email" => $email]);
     $user = $statement->fetch(PDO::FETCH_ASSOC);
@@ -25,8 +25,8 @@ function set_flash_message($name, $message) {
 }
 
 function add_user($email,$password) {
-	$pdo = new PDO("mysql:host=localhost;port=3307;dbname=tasks_db;", "root", "");
-	$sql = "INSERT INTO ttest (email,password) VALUES (:email,:password)";
+	$pdo = new PDO("mysql:host=localhost;port=3307;dbname=well;", "root", "");
+	$sql = "INSERT INTO new (email,password) VALUES (:email,:password)";
 	$statement = $pdo->prepare($sql);
 	$statement->execute([
 	"email" => $email,
@@ -39,8 +39,8 @@ return $pdo->lastInsertId();
 
 
 function login ($email, $password) {
-	$pdo = new PDO("mysql:host=localhost;port=3307;dbname=tasks_db;", "root", "");
-	$sql = "SELECT * FROM ttest WHERE email=:email";
+	$pdo = new PDO("mysql:host=localhost;port=3307;dbname=well;", "root", "");
+	$sql = "SELECT * FROM new WHERE email=:email";
     $statement = $pdo->prepare($sql);
     $statement->execute([
         'email'  => $email
@@ -49,11 +49,28 @@ function login ($email, $password) {
 
     if(password_verify($password, $result['password'])) {
         return $result;
-    } else {
+    } 
+    else {
         return false;
     }
 
 }
+
+
+
+
+function is_admin() 
+{
+	  if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin') {
+        return true;
+    } 
+    else {
+        return false;
+    }
+}
+
+
+
 
 
 function display_flash_message($name) {
